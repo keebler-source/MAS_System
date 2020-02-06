d1cb545f141381b9eb40487fe74ffeec9bae4bf9
gamedud22 <cokeandpepis@gmail.com>
2020-02-06 11:53:34 -0700

    SQL login function

--- MAS_System_/MAS_System_/Login.cs
+++ MAS_System_/MAS_System_/Login.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,10 +21,31 @@ namespace MAS_System_
 
         private void loginBtn_Click(object sender, EventArgs e)
         {
+
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MAS_System.mdf;Integrated Security=True;Connect Timeout=30");
+            //string qurey
+            string query = "SELECT * from dbo.[user] WHERE username='" + userNameTxt.Text.Trim() + "' AND password='" + passwrdTxt.Text.Trim() + "'";
+            //data adapter
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlCon);
+            /* in above line the program is selecting the whole data from table and the matching it with the user name and password provided by user. */
+            DataTable dt = new DataTable(); //this is creating a virtual table  
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                /* if the validation is true open sensor form */
+                Sensor sensor = new Sensor();
+                this.Hide();
+                sensor.Show();
+                sda.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username and or password");
+            }
+                       
+              
             
-           Sensor sensor = new Sensor();
-            this.Hide();
-            sensor.Show();
+           
             
             
         }
